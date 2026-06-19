69921809910b4940d04fc48f9c350307e5deebf3
Rishat Kagirov <rishatk87@gmail.com>
2026-06-19 17:10:08 +0300

    Init

---
+++ data.js
@@ -0,0 +1,276 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Танцевальный зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1918,
+      "initialViewParameters": {
+        "yaw": -3.0411130169860883,
+        "pitch": 0.043630132158918755,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.25860251013163094,
+          "pitch": 0.32485337187092966,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06007573945782774,
+          "pitch": 0.294304058691953,
+          "title": "Колонка",
+          "text": "Колонка с Bluetooth"
+        }
+      ]
+    },
+    {
+      "id": "1---",
+      "name": "Сцена актового зала",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1928,
+      "initialViewParameters": {
+        "yaw": -0.3699455603041226,
+        "pitch": -0.051908741446759166,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Актовый зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.2065816575661863,
+        "pitch": 0.43628444623175433,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.057818761696360355,
+          "pitch": 0.2882037088770524,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": 2.8226566422968116,
+          "pitch": -0.1605721311388102,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.48179617046022827,
+          "pitch": 0.2657996833253833,
+          "rotation": 5.497787143782138,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Коворкинг",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 1.0846084866215975,
+        "pitch": -0.0075090621151403525,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3695858923700719,
+          "pitch": 0.04841957741018632,
+          "rotation": 0,
+          "target": "4--"
+        },
+        {
+          "yaw": 0.5269137683726015,
+          "pitch": 0.07210385738815539,
+          "rotation": 7.0685834705770345,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "Корпус Д",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.7296395345286584,
+        "pitch": 0.3796412250183234,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8947204732898904,
+          "pitch": -0.0884341855912485,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.560968499438495,
+          "pitch": -0.08384025107346815,
+          "rotation": 0.7853981633974483,
+          "target": "5------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5------",
+      "name": "Штаб Союза студентов и аспирантов КНИТУ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0695649087155807,
+          "pitch": 0.5567602392222479,
+          "rotation": 5.497787143782138,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1537698548773196,
+          "pitch": 0.39931633265037014,
+          "title": "Стеллаж с наградами",
+          "text": "ТЕКСТ ТЕКСТ ТЕКСТ"
+        },
+        {
+          "yaw": -0.7064307646083474,
+          "pitch": 0.48690915133470725,
+          "title": "Стол переговоров",
+          "text": "Здесь принимаются важные для студенчества решения"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
