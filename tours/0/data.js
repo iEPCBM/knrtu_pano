08b93eceecce68d3e5cccf685f9b38d3ef62de6f
Rishat Kagirov <rishatk87@gmail.com>
2026-07-25 23:30:25 +0300

    Init

---
+++ tours/0/data.js
@@ -0,0 +1,55 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "Холл Корпуса А",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.866328313046525,
+          "pitch": 0.19052820118292502,
+          "title": "Выставка работ студентов ИТЛМПД",
+          "text": ""
+        },
+        {
+          "yaw": 1.7469531783438548,
+          "pitch": -0.04215110066568073,
+          "title": "Фотозона КНИТУ",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Корпус А",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ tours/5/data.js
@@ -0,0 +1,67 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--1---",
+      "name": "Пространство ДАС 1 - РСО",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.9276629960229084,
+          "pitch": 0.293672457939147,
+          "title": "Пространство РСО в ДАС 1",
+          "text": "Помимо мероприятий общежития, здесь также проходят мероприятия ШСО \"Технолог\" и Татарстанского регионального отделения Российских студенческих отрядов&nbsp;"
+        },
+        {
+          "yaw": 1.4450421757841045,
+          "pitch": 0.03816133109813791,
+          "title": "Культурник 2026",
+          "text": "<img src=\"/photos/thumbs/5_1.jpg\" style=\"width: 180px\"><p>Фото: Студсовет ДАС 1</p>"
+        },
+        {
+          "yaw": 1.8091297211949708,
+          "pitch": 0.03140622628448675,
+          "title": "Игротека \"Не игры\", 2026",
+          "text": "<img src=\"/photos/thumbs/5_2.jpg\" style=\"width: 180px\"><p>Фото: Студсовет ДАС 1</p>"
+        },
+        {
+          "yaw": -1.7523482763406015,
+          "pitch": 0.1692664391572869,
+          "title": "Навруз, 2026",
+          "text": "<img src=\"/photos/thumbs/5_3.jpg\" style=\"width: 180px\"><p>Фото: Студсовет ДАС 1</p>"
+        }
+      ]
+    }
+  ],
+  "name": "ДАС 1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
